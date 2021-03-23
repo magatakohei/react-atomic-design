aebd9002f3317e22914541b0956cdf88ec2c50e6
magako <qutianh@gmail.com>
2021-03-24 05:11:37 +0900

    context

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,7 +1,13 @@
+
+import { useContext } from "react";
 import styled from "styled-components";
+import { UserContext } from "../../providers/UserProvider";
 
 export const UserIconWithName = (props) => {
-  const { image, name, isAdmin } = props;
+  const { image, name } = props;
+  const { userInfo } = useContext(UserContext);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
+
   return (
     <SContainer>
       <SImg alt={name} width={160} height={160} src={image} />
@@ -16,13 +22,14 @@ const SContainer = styled.div`
 `;
 const SImg = styled.img`
   border-radius: 50%;
-`;
+  `
 const SName = styled.p`
   font-size: 18px;
   font-weight: bold;
   margin: 0;
   color: #40514e;
 `;
+
 const SEdit = styled.span`
   text-decoration: underline;
   color: #aaa;
